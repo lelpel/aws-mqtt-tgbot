e647bd39f8fb5d4d2b8b432809d84cd10cee262f
Mykhaylo Khimchenko <m.khm.ua@gmail.com>
2019-09-02 14:29:46 +0300

    some changes being made

--- bot.js
+++ bot.js
@@ -1,10 +1,38 @@
 const Telegraf = require("telegraf");
+require("dotenv").config();
+const AWSHelper = require("./aws");
+
+const config = require("./config.json");
+const { credentials, endpoint, clientId, will } = config;
+
 const bot = new Telegraf(process.env.TOKEN);
+
+const aws = new AWSHelper(
+  credentials,
+  credentials.region,
+  endpoint,
+  clientId,
+  will,
+  (topic, message) => {
+    Telegraf.reply(`Topic: ${topic}, message: ${message}`);
+  }
+);
 
 bot.start(({ reply, message }) => {
   reply(`Hello, ${message.from.username}`);
 });
 
-bot.command("get", Telegraf.reply("/get"));
+bot.on("message", ctx => {
+  aws.publish(ctx.message, "test3");
+});
+// bot.hears("restart", ({ reply, message }) => {
+//   reply(`Hello, ${message.from.username}`);
+// });
+
+bot.command("subscribe", ({ reply }) => {
+  aws.subscribe("test3", () => reply("Ошибка при подписке"));
+});
+
+bot.command("get", Telegraf.reply("Ну нажал ты кнопку дальше что"));
 
 bot.launch();
