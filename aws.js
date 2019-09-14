4cd936dbc66b708d5a23f93abca8d5ab4be7b691
Mykhaylo Khimchenko <m.khm.ua@gmail.com>
2019-09-14 15:12:40 +0300

    Commenting code. Refactioring .env

--- aws.js
+++ aws.js
@@ -2,34 +2,61 @@ const AWS = require("aws-sdk");
 const AWSMqttClient = require("aws-mqtt/lib/NodeClient");
 const config = require("./config");
 
+/** This class handles AWS Mqtt Server connections and messaging */
 class AwsMqtt {
+  /**
+   * @constructor
+   * @param {function} onMessage callback when message is recieved
+   */
   constructor(onMessage) {
     AWS.config.loadFromPath("./credentials.json");
 
     this.client = new AWSMqttClient({
-      region: AWS.config.region,
+      region: AWS.config.region, //AWS Region, e.g. 'us-east-1'
       credentials: AWS.config.credentials,
-      endpoint: config.aws.iot.endpoint,
+      endpoint: config.aws.iot.endpoint, // REST API endpoint
       clientId: "mqtt-client-" + Math.floor(Math.random() * 100000 + 1)
     });
 
+    /**
+     * Handle connections
+     */
     this.client.on("connect", () => {
       console.log("connected");
-      this.subscribe();
-      this.publish();
     });
 
+    /**
+     * Handle message recieval
+     */
     this.client.on("message", (topic, message) => {
       onMessage(topic, message);
       console.log(`Topic = ${topic}, message = ${message}`);
     });
   }
 
+  /**
+   * Sends message to AWS Server
+   *
+   * For debug purposes, method sends test message if arguments is not defined
+   * @param {string} message User's message
+   * @param {string} topic Message topic
+   */
   publish(message = config.test.message, topic = config.topics.test) {
     this.client.publish(topic, message);
   }
 
-  subscribe(topic = config.topics.test, cb = undefined) {
+  /**
+   * Subscribe to a topic
+   *
+   * @param {string} topic Topic
+   * @param {function} cb Callback: client is subscribed
+   */
+  subscribe(
+    topic = config.topics.test,
+    cb = (error, granted) => {
+      error ? console.log(error) : console.log(granted);
+    }
+  ) {
     this.client.subscribe(topic, cb);
   }
 }
--- bot.js
+++ bot.js
@@ -1,24 +1,39 @@
 require("dotenv").config();
 
-const AwsMqtt = require("./aws");
-
 const Telegraf = require("telegraf");
 const commandParts = require("telegraf-command-parts");
-const bot = new Telegraf(process.env.TOKEN2);
+
+/**
+ * Token obtained from bot father
+ */
+const bot = new Telegraf(process.env.TOKEN);
 bot.use(commandParts());
 
-let chatId;
+const AwsMqtt = require("./aws");
 
 const aws = new AwsMqtt((topic, message) => {
-  console.log(message);
-  console.log(`Topic: ${topic}, message: ${message}`);
+  //on message recieved
   bot.telegram.sendMessage(chatId, `Topic: ${topic}, message: ${message}`);
+  console.log(`Topic: ${topic}, message: ${message}`);
 });
 
+// BOT LIFECYCLE
+
+/**
+ * Handles bot's start. Replies with user's telegram @-handle
+ */
 bot.start(({ reply, message }) => {
   reply(`Hello, ${message.from.username}`);
 });
 
+let chatId; //stores current chat id to forward messages from AWS Server
+
+/**
+ * Handles subscription command, i.e. "/sub topic"
+ *
+ * Subscribes to default topic if user's topic is not defined
+ * Replies with success/error message after subscription
+ */
 bot.command("sub", ({ reply, state, message }) => {
   const args = state.command.splitArgs;
   const topic = args
@@ -27,19 +42,27 @@ bot.command("sub", ({ reply, state, message }) => {
 
   aws.subscribe(topic, (error, granted) =>
     granted
-      ? reply(`Successfully subscribed to topic ${topic}`)
-      : reply(`Error subscribing to topic ${topic}, errmsg: ${error}`)
+      ? reply(`Successfully subscribed to topic **${topic}**`)
+      : reply(`Error subscribing to topic **${topic}**, errmsg: ${error}`)
   );
 
   chatId = message.chat.id;
 });
 
+/**
+ * Handles message sending command, i.e. "/send topic;message"
+ *
+ * Replies with sent message
+ */
 bot.command("send", ({ reply, state }) => {
   const [topic, message] = state.command.args.split(";");
   aws.publish(message, topic);
-  reply(`Sent. Topic ${topic}, message: ${message}`);
+  reply(`Sent. Topic **${topic}**, message: __${message}__`);
 });
 
+/**
+ * Checks if bot works
+ */
 bot.command("heartbeat", ({ reply, message }) => {
   reply(`Hello, ${message.from.username}`);
 });
